c3b8892308002f5e3ccd63a01a3883ad3d9afd9a
Luca Spirito <luca.spirito@itsincom.it>
2025-03-13 16:56:40 +0100

    Creazioni dipendenti

---
+++ conrtrolloAccessi/DATA/Azienda.cs
@@ -0,0 +1,9 @@
+﻿namespace conrtrolloAccessi.DATA
+{
+    public class Azienda
+    {
+        public int Id { get; set; }
+        public required string Nome { get; set; }
+        public required List<Dipendente> Dipendenti { get; set; }
+    }
+}
