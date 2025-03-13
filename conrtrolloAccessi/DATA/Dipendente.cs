b226bdab0832b2b160404f08dbd09cf1b3b2f4d2
Luca Spirito <luca.spirito@itsincom.it>
2025-03-13 17:10:16 +0100

    Entità

--- conrtrolloAccessi/DATA/Dipendente.cs
+++ conrtrolloAccessi/DATA/Dipendente.cs
@@ -5,8 +5,7 @@
         public int Id { get; set; }
         public required string Nome { get; set; }
         public required string Cognome { get; set; }
-        public required string CodiceFiscale { get; set; }
-        public required string Email { get; set; }
+        public required string Mansione { get; set; }
 
 
     }
